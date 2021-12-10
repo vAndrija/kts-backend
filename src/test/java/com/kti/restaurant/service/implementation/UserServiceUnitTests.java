a0334ad70ea8626a2fa48680e2ef77bb34455a5b
vAndrija <andrija.vojnovic@uns.ac.rs>
2021-12-10 23:46:10 +0100

    unit testing of userService finished

--- src/test/java/com/kti/restaurant/service/implementation/UserServiceUnitTests.java
+++ src/test/java/com/kti/restaurant/service/implementation/UserServiceUnitTests.java
@@ -1,6 +1,7 @@
 package com.kti.restaurant.service.implementation;
 
 
+import com.kti.restaurant.exception.BadTokenException;
 import com.kti.restaurant.exception.MissingEntityException;
 import com.kti.restaurant.model.Admin;
 import com.kti.restaurant.model.User;
@@ -20,11 +21,14 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
 
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource("classpath:application-test.properties")
@@ -56,7 +60,7 @@ public class UserServiceUnitTests {
     @Test
     public void loadUserByUsername_ValidUsername_ExistingUserDetails() throws Exception {
         UserDetails userDetails = userService.loadUserByUsername("andrija@vojnvo.com");
-        assertEquals( "andrija@vojnvo.com",userDetails.getUsername());
+        assertEquals("andrija@vojnvo.com", userDetails.getUsername());
     }
 
     @Test
@@ -70,8 +74,56 @@ public class UserServiceUnitTests {
     public void sendResetToken_ValidEmail() throws Exception {
         when(confirmationTokenRepository.save(any()))
                 .thenAnswer(a -> a.getArgument(0));
-        assertDoesNotThrow(()->{userService.sendResetToken("andrija@vojnvo.com");});
+        assertDoesNotThrow(() -> {
+            userService.sendResetToken("andrija@vojnvo.com");
+        });
     }
 
+    @Test
+    public void sendResetToken_InvalidEmail_ThrowsMissingEntityException() {
+        assertThrows(MissingEntityException.class, () -> {
+            userService.sendResetToken("andrija@vojnovic.com");
+        });
+    }
+
+    @Test
+    public void resetPasswordDone_ValidData() throws Exception {
+        User user = new Admin("Vojnovic", "Andrija", "213123123", "andrija@vojnvo.com", "21312311");
+        ConfirmationToken confirmationToken = new ConfirmationToken(user);
+        when(confirmationTokenRepository.findByConfirmationToken(any()))
+                .thenAnswer(a -> confirmationToken);
+        userService.resetPasswordDone("andrija@vojnvo.com", confirmationToken.getConfirmationToken(), "modemmodem123");
+        verify(confirmationTokenRepository, times(1)).delete(confirmationToken);
+        verify(userRepository, times(1)).save(any());
+    }
+
+    @Test
+    public void resetPasswordDone_UnexistingToken_BadTokenException() throws Exception {
+        User user = new Admin("Vojnovic", "Andrija", "213123123", "andrija@vojnvo.com", "21312311");
+        ConfirmationToken confirmationToken = new ConfirmationToken(user);
+        when(confirmationTokenRepository.findByConfirmationToken(any()))
+                .thenAnswer(a -> null);
+        BadTokenException exception = assertThrows(BadTokenException.class, () -> {
+            userService.resetPasswordDone("andrija@vojnvo.com", confirmationToken.getConfirmationToken(), "modemmodem123");
+        });
+        assertEquals("Bad token", exception.getMessage());
+        verify(confirmationTokenRepository, times(0)).delete(confirmationToken);
+        verify(userRepository, times(0)).save(any());
+    }
+
+    @Test
+    public void resetPasswordDone_ExpiredToken_BadTokenException() throws Exception {
+        User user = new Admin("Vojnovic", "Andrija", "213123123", "andrija@vojnvo.com", "21312311");
+        ConfirmationToken confirmationToken = new ConfirmationToken(user);
+        when(confirmationTokenRepository.findByConfirmationToken(any()))
+                .thenAnswer(a -> confirmationToken);
+        confirmationToken.setCreatedDate(new Date(new Date().getTime() - 1000 * 60 * 60 * 31));
+        BadTokenException exception = assertThrows(BadTokenException.class, () -> {
+            userService.resetPasswordDone("andrija@vojnvo.com", confirmationToken.getConfirmationToken(), "modemmodem123");
+        });
+        assertEquals("Token expired", exception.getMessage());
+        verify(confirmationTokenRepository, times(0)).delete(confirmationToken);
+        verify(userRepository, times(0)).save(any());
+    }
 
 }
