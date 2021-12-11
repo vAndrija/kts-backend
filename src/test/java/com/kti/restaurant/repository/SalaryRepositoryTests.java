dc80de73a906ce6c2d6f3e4528217bd9d428747b
nikolinat <nikolinatosic999@gmail.com>
2021-12-11 23:16:25 +0100

    added parameterized tests for repository

--- src/test/java/com/kti/restaurant/repository/SalaryRepositoryTests.java
+++ src/test/java/com/kti/restaurant/repository/SalaryRepositoryTests.java
@@ -3,8 +3,11 @@ package com.kti.restaurant.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
@@ -17,22 +20,32 @@ public class SalaryRepositoryTests {
 
 	@Autowired
 	private SalaryRepository salaryRepository;
-	
-	@Test
-	public void findSalaryForDate_ValidDate_ExistingUser() {
-		Salary salary = salaryRepository.findSalaryForDate(LocalDate.parse("2021-11-19"), 1);
-		assertEquals(Double.valueOf(45000.00), salary.getValue());
+
+
+	@ParameterizedTest
+	@MethodSource("validDatesForFindSalaryForDate")
+	public void findSalaryForDate(LocalDate date, int userId, double expected) {
+		Salary salary = salaryRepository.findSalaryForDate(date, userId);
+		assertEquals(expected, salary.getValue());
 	}
-	
-	@Test
-	public void findSalaryForDate_InvalidDate_Null() {
-		Salary salary = salaryRepository.findSalaryForDate(LocalDate.parse("2021-11-15"), 1);
-		assertEquals(null, salary);
+
+	private static Stream<Arguments> validDatesForFindSalaryForDate() {
+		return Stream.of(
+				Arguments.of(LocalDate.parse("2021-11-19"), 1, Double.valueOf(45000.00))
+		);
 	}
-	
-	@Test
-	public void findSalaryForDate_InvalidUserIdKey_Null() {
-		Salary salary = salaryRepository.findSalaryForDate(LocalDate.parse("2021-11-19"), 100);
-		assertEquals(null, salary);
+
+	@ParameterizedTest
+	@MethodSource("invalidDatesForFindSalaryForDate")
+	public void findSalaryForDateInvalid(LocalDate date, int userId, Object expected) {
+		Salary salary = salaryRepository.findSalaryForDate(date, userId);
+		assertEquals(expected, salary);
+	}
+
+	private static Stream<Arguments> invalidDatesForFindSalaryForDate() {
+		return Stream.of(
+				Arguments.of(LocalDate.parse("2021-11-15"), 1, null),
+				Arguments.of(LocalDate.parse("2021-11-19"), 100, null)
+		);
 	}
 }
