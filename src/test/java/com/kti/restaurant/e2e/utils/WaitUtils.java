28953e1095086b28f41efbbc91b3f71020091644
jojev <jo.jevtic999@gmail.com>
2022-01-11 21:31:13 +0100

    added e2e test for login

---
+++ src/test/java/com/kti/restaurant/e2e/utils/WaitUtils.java
@@ -0,0 +1,46 @@
+package com.kti.restaurant.e2e.utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class WaitUtils {
+
+	public static boolean urlWait(WebDriver driver, String url, int wait) {
+		return new WebDriverWait(driver, wait).until(ExpectedConditions.urlToBe(url));
+	}
+
+	public static boolean titleWait(WebDriver driver, String title, int wait) {
+		return new WebDriverWait(driver, wait).until(ExpectedConditions.titleIs(title));
+	}
+
+	public static WebElement visibilityWait(WebDriver driver, WebElement element, int wait) {
+		return new WebDriverWait(driver, wait).until(ExpectedConditions.visibilityOf(element));
+	}
+	
+	public static List<WebElement> visibilityWait(WebDriver driver, By locator, int wait) {
+		return new WebDriverWait(driver, wait).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
+	}
+
+	public static WebElement presenceWait(WebDriver driver, By locator, int wait) {
+		return new WebDriverWait(driver, wait).until(ExpectedConditions.presenceOfElementLocated(locator));
+	}
+
+	public static WebElement clickableWait(WebDriver driver, WebElement element, int wait) {
+		return new WebDriverWait(driver, wait).until(ExpectedConditions.elementToBeClickable(element));
+	}
+
+	public static boolean isPresent(WebDriver driver, By locator) {
+
+		try {
+			return driver.findElement(locator).isDisplayed();
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+}
